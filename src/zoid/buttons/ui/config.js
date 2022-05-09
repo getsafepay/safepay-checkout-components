6de11ba48b3cf0de40f63a2dc2fae214f80a0ec8
Ziyad Parekh <ziyad.parekh@gmail.com>
2022-05-09 14:11:46 +0500

    fix button ui

--- src/zoid/buttons/ui/config.js
+++ src/zoid/buttons/ui/config.js
@@ -2,21 +2,21 @@ import { BUTTON_SIZES } from "../../../constants/index.js"
 
 export const BUTTON_SIZE_STYLE = {
   [BUTTON_SIZES.SMALL]: {
-    defaultWidth:    150,
-    defaultHeight:   92,
-    minWidth:        150,
+    defaultWidth:    200,
+    defaultHeight:   108,
+    minWidth:        200,
     maxWidth:        320,
     minHeight:       48,
-    maxHeight:       100
+    maxHeight:       150
   },
 
   [BUTTON_SIZES.MEDIUM]: {
     defaultWidth:      250,
-    defaultHeight:     100,
+    defaultHeight:     116,
     minWidth:          250,
     maxWidth:          400,
     minHeight:         56,
-    maxHeight:         108
+    maxHeight:         150
   },
 
   [BUTTON_SIZES.LARGE]: {
@@ -25,6 +25,6 @@ export const BUTTON_SIZE_STYLE = {
     minWidth:          350,
     maxWidth:          500,
     minHeight:         60,
-    maxHeight:         132
+    maxHeight:         150
   }
 }
